3bc0d0eae13256fdf0d14c1a36fdb56a3c1e7448
NguyenPhuLam1997 <centsdeath@gmail.com>
2018-04-10 08:13:08 +0700

    Bài TH2

---
+++ BaiTH2/src/Luuclass/Luuclass.java
@@ -0,0 +1,56 @@
+package Luuclass;
+
+/**
+ *
+ * @author bang
+ */
+public class Luuclass {
+    int STT;
+    String name;
+    String gioiTinh;
+    String namSinh;
+    public int getSTT(){
+        return this.STT;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getGioiTinh(){
+        return this.gioiTinh;
+    }
+    public String getNamSinh(){
+        return this.namSinh;
+    }
+    public void STT(int STT){
+        this.STT = STT;
+    }
+    public void GioiTinh(String gioiTinh){
+        this.gioiTinh = gioiTinh;
+    }
+    public void Name(String name){
+        this.name = name;
+    }
+    public void NamSinh(String namSinh){
+        this.namSinh = namSinh;
+    }
+
+    void getName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getSTT(Luuclass sv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getNamSinh(Luuclass sv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getGioiTinh(Luuclass sv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getName(Luuclass sv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ BaiTH2/src/baith2/BaiTH2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baith2;
+
+import static sun.security.jgss.GSSUtil.login;
+
+/**
+ *
+ * @author admin
+ */
+public class BaiTH2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new login().setVisible(true);
+    }
+    
+}
