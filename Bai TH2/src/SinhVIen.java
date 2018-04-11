8b86f1520b1a10ea57537045eb82545778f20dbc
NguyenPhuLam1997 <centsdeath@gmail.com>
2018-04-11 13:55:44 +0700

    Bài Thực Hành Số 2 Hoàn Chỉnh

---
+++ Bai TH2/src/SinhVIen.java
@@ -0,0 +1,66 @@
+public class SinhVIen {
+    private String HoTen;
+    private int NamSinh;
+    private String GioiTinh;
+
+    /**
+     * @return the HoTen
+     */
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    /**
+     * @param HoTen the HoTen to set
+     */
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    /**
+     * @return the NamSinh
+     */
+    public int getNamSinh() {
+        return NamSinh;
+    }
+
+    /**
+     * @param NamSinh the NamSinh to set
+     */
+    public void setNamSinh(int NamSinh) {
+        this.NamSinh = NamSinh;
+    }
+
+    /**
+     * @return the GioiTinh
+     */
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    /**
+     * @param GioiTinh the GioiTinh to set
+     */
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+
+    void getHoTen(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void getNamSinh(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void getGioiTinh(String nam) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void getNamSinh(int NamSinh) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
