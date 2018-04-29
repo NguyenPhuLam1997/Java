4714f45d8f20aeb1fe54809a10fbfeed15599c51
NguyenPhuLam1997 <centsdeath@gmail.com>
2018-04-29 13:45:12 +0700

    BaiTH4

---
+++ BaiTH41/SinhVien.java
@@ -0,0 +1,68 @@
+package baith4;
+
+public class SinhVien 
+{
+    private String MSVV;
+    private String HoTen;
+    private String NamSinh;
+    private String Lop;
+
+    /**
+     * @return the MSVV
+     */
+    public String getMSVV() {
+        return MSVV;
+    }
+
+    /**
+     * @param MSVV the MSVV to set
+     */
+    public void setMSVV(String MSVV) {
+        this.MSVV = MSVV;
+    }
+
+    /**
+     * @return the HoTen
+     */
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    /**
+     * @param HoTen the HoTen to set
+     */
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    /**
+     * @return the NamSinh
+     */
+    public String getNamSinh() {
+        return NamSinh;
+    }
+
+    /**
+     * @param NamSinh the NamSinh to set
+     */
+    public void setNamSinh(String NamSinh) {
+        this.NamSinh = NamSinh;
+    }
+
+    /**
+     * @return the Lop
+     */
+    public String getLop() {
+        return Lop;
+    }
+
+    /**
+     * @param Lop the Lop to set
+     */
+    public void setLop(String Lop) {
+        this.Lop = Lop;
+    }
+    
+
+    
+}
